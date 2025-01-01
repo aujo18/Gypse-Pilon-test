36ab25c18e5e6721fd9b09d6d3e26826422918ce
aujo18 <89912606+aujo18@users.noreply.github.com>
2025-01-01 10:14:11 -0500

    Update Footer.tsx

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,17 +1,17 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 import cn from 'classnames'
 import s from './Footer.module.css'
 import { Container } from '@components/ui'
 import { useTranslations } from 'next-intl'
 
 const Footer: FC = () => {
-  const t = useTranslations()
+  const t = useTranslations('Footer')
   const currentYear = new Date().getFullYear()
 
   return (
     <footer className={s.root}>
       <Container>
-        <p className={s.text}>{t('Footer.text').replace('{year}', currentYear.toString())}</p>
+        <p className={s.text}>{t('text').replace('{year}', currentYear.toString())}</p>
       </Container>
     </footer>
   )
